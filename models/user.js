ff196c3cbb8648fa7ec0594f6a8bdb5d56e3dd89
Lohithpattu <cse20lohithkumar23@nehrucolleges.org>
2023-09-11 10:02:03 +0530

    first version

---
+++ models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  about: { type: String },
+  tags: { type: [String] },
+  joinedOn: { type: Date, default: Date.now },
+  points:{type:Number,default:0},
+  numberOfanswer: { type: Number, default:0}
+});
+
+export default mongoose.model("User", userSchema);
